89d19aa0b3b0b484a9ae1daf279c52b1511d74d1
github username <github email address>
2019-02-04 18:29:00 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+
+class Driver {
+  constructor(name) {
+  this.name = name;
+  this.id = ++driverId;
+
+    store.drivers.push(this);
+  };
+
+}
+
+let passengerId = 0
+class Passenger {
+  constructor(name) {
+  this.name = name;
+  this.id = ++passengerId;
+
+    store.passengers.push(this);
+  };
+}
+
+let tripId = 0
+class Trip {
+  constructor(driverId, passengerId) {
+  this.driverId = driverId;
+  this.passengerId = passengerId;
+  this.id = ++tripId;
+
+    store.trips.push(this);
+  };
+
+  // driver(trip) {
+  // return trip.driver
+  // }
+}
